7dc44bc9e4052269db46100a7a2f95194b09e60c
Jamison Dance <jergason@gmail.com>
2013-01-04 19:30:06 -0700

    make socketio work

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
 
-server.listen(3005);
+server.listen(3005)
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+  res.sendfile(__dirname + '/index.html')
+})
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  socket.emit('news', { hello: 'world' })
   socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+    console.log(data)
+  })
+})
