bfb44d26f063811a42f8f546be0b3ca5deccfc17
Jamison Dance <jergason@gmail.com>
2013-01-04 19:23:03 -0700

    add socketio

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ var io = socketio.listen(server)
 
 server.use(express.static(__dirname + '/public'))
 
+io.sockets.on('connection', function (socket) {
+  socket.emit('test', { hurp: 'durp' })
+})
+
 server.listen(3005)
