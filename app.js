2767556cf3ce4cc6641a0581f7e80a7774d7c53c
Jamison Dance <jergason@gmail.com>
2013-01-04 19:36:44 -0700

    add semicolons

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ server.listen(3005)
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/public/index.html');
-});
+  res.sendfile(__dirname + '/public/index.html')
+})
 
 io.sockets.on('connection', function (socket) {
   socket.on('message', function (message) {
     // echo
-    socket.emit('message', message);
-  });
-});
+    socket.emit('message', message)
+  })
+})
